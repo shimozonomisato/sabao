76fcd9eeb8d31acb1efb612b71d37d09dff4f80a
shimozonomisato <zazizuzezono@gmail.com>
2018-01-01 16:52:41 +0900

    first commit

---
+++ app/Http/Controllers/HelloController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class HelloController extends Controller
+{
+
+   public function index()
+   {
+       $data = [
+           'msg'=>'お名前を入力下さい。',
+       ];
+       return view('hello.index', $data);
+   }
+
+   public function post(Request $request)
+   {
+//       var_export('test001');
+       $msg = $request->msg;
+       $data = [
+           'msg'=>'こんにちは、' . $msg . 'さん！',
+       ];
+       return view('hello.index', $data);
+   }
+
+}
