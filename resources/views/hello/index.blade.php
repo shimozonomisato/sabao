d8f5a514549cef4e81a9d620a250b7fcaa7bcd6e
shimozonomisato <zazizuzezono@gmail.com>
2018-01-01 16:26:50 +0900

    first commit

---
+++ resources/views/hello/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Hello</title>
+    <style>
+    body {font-size:16px; color:#999;}
+    h1 {font-size:50px; text-align:right; color:#EF75BC; margin:-20px 0px -30px 0px; letter-spacing:-4px;}
+    </style>
+  </head>
+  <body>
+   <h1>Blade/Index</h1>
+   <p>{{$msg}}</p>
+   <form method="POST" action="/hello">
+       {{ csrf_field() }}
+       <input type="text" name="msg">
+       <input type="submit">
+   </form>
+</body>
+</html>
