f7d131b53c50344d2810553e6b5e66b99ba3e55a
shimozonomisato <zazizuzezono@gmail.com>
2018-01-06 15:05:48 +0900

    whileの例、phpスクリプトを使う

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -10,16 +10,17 @@
   </head>
   <body>
    <h1>Blade/Index</h1>
-   <p>&#064;forディレクティブの例_$loopを使う</p>
-   @foreach ($data as $item)
-     @if ($loop->first)
-     <p>＊データ一覧</p>
-     <ul>
-     @endif
-     <li>No,{{$loop->iteration}}. {{$item}}</li>
-     @if ($loop->last)
-     </ul><p>-----ここまで</p>
-     @endif
-     @endforeach
+   <p>&#064;whileの例、phpスクリプトを使う</p>
+   <ol>
+     @php
+     $counter = 0;
+     @endphp
+     @while ($counter < count($data))
+      <li>{{$data[$counter]}}</li>
+     @php
+     $counter++;
+     @endphp
+     @endwhile
+   </ol>
   </body>
 </html>
