e4203496dacec33f5acaa05678c3b5a24ca9dfc8
shimozonomisato <zazizuzezono@gmail.com>
2018-01-11 13:37:13 +0900

    ビューコンポーザーのクラスを定義しbootで設定

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -10,12 +10,7 @@ class HelloController extends Controller
 
    public function index()
    {
-       $data = [
-         ['name'=>'山田太郎', 'mail'=>'taro@ymada'],
-         ['name'=>'田中もか', 'mail'=>'moka@nya'],
-         ['name'=>'雉子さばお', 'mail'=>'saba@nya']
-       ];
-       return view('hello.index', ['data'=>$data]);
+       return view('hello.index', ['message'=>'もか!']);
    }
 
 }
