4b3b4f936ee70d254d2ee99824042089e7f29b11
shimozonomisato <zazizuzezono@gmail.com>
2018-01-13 22:11:55 +0900

    ミドルウェアの実行

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,19 @@
 <?php
 
+use App\Http\Middleware\HelloMiddleware;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
+| 
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,6 +21,5 @@ Route::get('/', function () {
 
 
 
-
-Route::get('hello', 'HelloController@index');
-Route::post('hello', 'HelloController@post');
+Route::get('hello', 'HelloController@index')
+  ->middleware(HelloMiddleware::class);
