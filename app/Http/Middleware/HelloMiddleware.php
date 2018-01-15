c2d66bbacf4d7174d175e75ed6f7cb88f40abdcb
zono <>
2018-01-15 19:46:32 +0900

    first commit,ミドルウェアの作成

---
+++ app/Http/Middleware/HelloMiddleware.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class HelloMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed 
+     */
+    public function handle($request, Closure $next)
+    {
+
+        $data = [
+          ['name'=>'taro', 'mail'=>'taro@yamada'],
+          ['name'=>'moca', 'mail'=>'moca@tanaka'],
+          ['name'=>'sabao', 'mail'=>'sabao@saki'],
+        ];
+        $request->merge(['data'=>$data]);
+        return $next($request);
+    }
+}
