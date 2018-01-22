115be3bbbb9a7a03dbaa14c339b939c2165c5293
zono <osato@mbp.local>
2018-01-22 19:20:28 +0900

    項目ごとにエラーメッセージを表示する

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -11,6 +11,9 @@
 
   <p>{{$msg}}</p>
   @if (count($errors) > 0)
+  <p>入力に問題があります。再入力してください。</p>
+  @endif
+
   <div>
     <ul>
       @foreach ($errors->all() as $error)
@@ -22,9 +25,18 @@
   <table>
     <form action="/hello" method="post">
       {{ csrf_field() }}
-      <tr><th>name: </th><td><input type="text" name="name"></td></tr>
-      <tr><th>mail: </th><td><input type="text" name="mail"></td></tr>
-      <tr><th>age: </th><td><input type="text" name="age"></td></tr>
+      @if ($errors->has('name'))
+      <tr><th>ERROR</th><td>{{$errors->first('name')}}</td></tr>
+      @endif
+      <tr><th>name: </th><td><input type="text" name="name" value="{{old('name')}}"></td></tr>
+      @if ($error->has('mail'))
+      @endif
+      <tr>ERROR<th></th><td>{{$errors->first('mail')}}</td></tr>
+      <tr><th>mail: </th><td><input type="text" name="mail" value="{{old('mail')}}"></td></tr>
+      @if ($errors->has('age'))
+      <tr><th>ERROR</th><td{{$errors->first('age')}}></td></tr>
+      @endif
+      <tr><th>age: </th><td><input type="text" name="age" value="{{old('age')}}"></td></tr>
       <tr><th></th><td><input type="submit" value="send"></td></tr>
     </form>
   </table>
