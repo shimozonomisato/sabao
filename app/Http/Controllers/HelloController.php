5daf112d686b244b06a896c564111fa438d641ed
zono <osato@mbp.local>
2018-01-27 15:02:44 +0900

    クッキーを利用する

--- app/Http/Controllers/HelloController.php
+++ app/Http/Controllers/HelloController.php
@@ -12,48 +12,26 @@ class HelloController extends Controller
 
    public function index(Request $request)
    {
-     $validator = Validator::make($request->query(), [
-       'id' => 'required',
-       'pass' => 'required',
-     ]);
-     if($validator->fails()) {
-       $msg = 'クエリーに問題があります。';
+     if($request->hasCookie('msg'))
+     {
+       $msg = 'Cookie: '. $request->cookie('msg');
      } else {
-       $msg = 'ID/PASS を受け付けました。フォームを入力ください。';
+       $msg = '*クッキーはありません';
      }
-       return view('hello.index', ['msg'=>$msg,]);
+     return view('hello.index', ['msg'=> $msg]);
    }
 
 
    public function post(Request $request)
    {
-     $rules = [
-       'name' => 'required',
-       'mail' => 'email',
-       'age' => 'numeric',
+     $validate_rule = [
+       'msg' => 'required',
      ];
-     $messages = [
-       'name.required' => '名前は必ず記入してください。',
-       'mail.email' => 'メールアドレスが必要です。',
-       'age.numeric' => '年齢を整数で記入してください。',
-       'age.min' => '年齢はゼロ歳以上で記入してください。',
-       'age.max' => '年齢は200歳以下で記入ください。',
-     ];
-     $validator = Validator::make($request->all(), $rules, $messages);
-
-     $validator->sometimes('age', 'min:0', function($input){
-       return !is_int($input->age);
-     });
-     $validator->sometimes('age', 'max:200', function($input) {
-       return !is_int($input->age);
-     });
-
-     if($validator->fails()) {
-       return redirect('/hello')
-        ->withErrors($validator)
-        ->withInput();
-     }
-       return view('hello.index', ['msg'=>'正しく入力されました！']);
+     $this->validate($request, $validate_rule);
+     $msg = $request->msg;
+     $response = new Response(view('hello.index', ['msg'=> '「'. $msg. '」をクッキーに保存しました。']));
+     $response->cookie('msg', $msg, 100);
+     return $response;
    }
 
 }
